6c61adaa4617b710e3c0d7e479a1f947129e45af
lvntky <klevent1903@gmail.com>
2023-09-07 00:43:26 +0300

    [kernel/feature] basic init

---
+++ kernel/include/kernel.h
@@ -0,0 +1,6 @@
+#ifndef __KERNEL_H__
+#defgine __KERNEL_H__
+
+void kernel_main(void);
+
+#endif
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,6 +1,5 @@
-void kernel_main(void) {
-	while(1) {
-	
-		
-	}
-} 
+#include "./include/kernel.h"
+
+void kernel_main() {
+
+}
