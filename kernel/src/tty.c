a096a59d27807f7c7552febad7db3ba6153c85f3
lvntky <klevent1903@gmail.com>
2023-09-08 18:03:45 +0300

    [feature] basic VGA init

--- kernel/src/tty.c
+++ kernel/src/tty.c
@@ -1,12 +1,33 @@
 #include "./tty.h"
 
+uint16_t *vid_mem = (uint16_t *)(VIDEO_MEM_ADDRESS);
+
 void terminal_init(void)
 {
-	volatile char *vid_mem = (volatile char *)(VIDEO_MEM_ADDRESS);
-	unsigned int vid_mem_area = VGA_WIDTH * VGA_HEIGHT;
-
-	for (int i = 0; i < vid_mem_area; i++) {
-		vid_mem[i] = '#';
-		vid_mem[i + 1] = 0;
+	size_t vga_area = VGA_WIDTH * VGA_HEIGHT;
+	for (size_t i = 0; i < vga_area; i++) {
+		vid_mem[i] = terminal_putchar(' ', 0);
 	}
+	terminal_write(TERMINAL_MESSAGE, 4);
+}
+
+uint16_t terminal_putchar(char c, char color)
+{
+	return (color << 8) | c;
+}
+
+void terminal_write(char *c, char color)
+{
+	for (size_t i = 0; i < strlen(c); i++) {
+		vid_mem[i] = terminal_putchar(c[i], color);
+	}
+}
+
+int strlen(char *str)
+{
+	int len = 0;
+	while (str[len]) {
+		len++;
+	}
+	return len;
 }
