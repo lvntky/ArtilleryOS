fe9b46bac37c4b7ce3eb2516d961001c18f40caf
lvntky <klevent1903@gmail.com>
2023-09-17 18:40:26 +0300

    [script] debug helper script

---
+++ scripts/debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start QEMU with the kernel image and wait for GDB connection
+qemu-system-x86_64 -hda ./bin/artillery.bin -gdb tcp::1234 -S &
+
+# Start GDB and load symbols, then continue execution
+gdb -ex "target remote localhost:1234" \
+    -ex "add-symbol-file ./build/kernel_and_bootloader_combined.o 0x100000" \
