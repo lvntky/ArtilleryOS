e69df870cd7755e6de739ba780c97ba7650e4a9e
lvntky <klevent1903@gmail.com>
2023-09-19 19:06:11 +0300

    [kernel] implicit declerations has deleted

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -5,18 +5,8 @@
 #include <ll_io.h>
 #include "../include/interrupt.h"
 
-int test_zero()
-{
-	int a = 5;
-	int b = 0;
-
-	return a / b;
-}
 void kernel_main(void)
 {
 	terminal_init();
 	init_gdt();
-	init_interrupt_manager(0x20);
-	interrupt_activate();
-	int result = 5 / 0;
 }
