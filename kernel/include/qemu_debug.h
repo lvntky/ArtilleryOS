934e79be9b5a4acdb1889f10578150eb721f3a63
lvntky <klevent1903@gmail.com>
2023-10-16 10:14:33 +0300

    [feature] debug console for qemu

---
+++ kernel/include/qemu_debug.h
@@ -0,0 +1,13 @@
+#ifndef _QEMU_DEBUG_H
+#define _QEMU_DEBUG_H
+
+#define QEMU_LOG_SERIAL_PORT    0x3f8
+#define QEMU_DEBUGCON_PORT  0xe9
+
+unsigned int qemu_init_debug();
+void qemu_write_string(const char *message);
+void qemu_write_char(const char);
+void debugcon_write_string(const char* message);
+void debugcon_write_char(const char c);
+
+#endif
