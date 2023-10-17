0ab790fedbb872c326e7014015752c8e9006f474
lvntky <klevent1903@gmail.com>
2023-10-17 13:31:57 +0300

    [bugfix] partial fix multiboot info

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,9 +1,3 @@
-MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
-MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
-MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
-MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
-MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
-
 [BITS 32]
 global start
 start:
@@ -20,16 +14,16 @@ extern bss
 extern end
 
 mboot:
-    dd  MBOOT_HEADER_MAGIC      ; GRUB will search for this value on each
-                                ; 4-byte boundary in your kernel file
-    dd  MBOOT_HEADER_FLAGS      ; How GRUB should load your file / settings
-    dd  MBOOT_CHECKSUM          ; To ensure that the above values are correct
+    align 4
+    MULTIBOOT_MAGIC     equ 0x1BADB002
+    MULTIBOOT_FLAGS     equ 0x03
+    MULTIBOOT_CHECKSUM  equ -MULTIBOOT_MAGIC-MULTIBOOT_FLAGS
     
-    dd  mboot                   ; Location of this descriptor
-    dd  code                    ; Start of kernel '.text' (code) section.
-    dd  bss                     ; End of kernel '.data' section.
-    dd  end                     ; End of kernel.
-    dd  start                   ; Kernel entry point (initial EIP).
+    dd MULTIBOOT_MAGIC  
+    dd MULTIBOOT_FLAGS
+    dd MULTIBOOT_CHECKSUM
+
+
 
 extern kernel_main
 stublet:
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,6 +1,6 @@
 #include "../include/kernel.h"
 
-void kernel_main(multiboot_info_t *mbi)
+void kernel_main()
 {
 	terminal_init();
 	qemu_init_debug();
@@ -9,19 +9,6 @@ void kernel_main(multiboot_info_t *mbi)
 	timer_init(50);
 	keyboard_init();
 	get_cpu_info();
-
-	uint32_t mbi_flags = mbi->flags;
-	uint32_t mbi_lower = mbi->mem_lower;
-	uint32_t mbi_upper = mbi->mem_upper;
-
-	printf("flags: 0x%x\n", mbi_flags);
-	printf("lower memory: %u kb\n",
-	       mbi_lower); // Use %u for unsigned integers
-	printf("upper memory: %u kb\n",
-	       mbi_upper); // Use %u for unsigned integers
-
-	printf("mmap_length: %u\n", mbi->mmap_length);
-	printf("mmap_addr: %u\n", mbi->mmap_addr);
 
 	// set_mode(320, 200, 8);
 	// clear_screen_withcolor(0x04);
