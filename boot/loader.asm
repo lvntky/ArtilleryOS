52eab258b7f5b944bbe1ee989be80f2689c95ad3
lvntky <klevent1903@gmail.com>
2023-11-10 10:28:52 +0300

    [refactor] more clean structure for bootloader

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,57 +1,42 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                   ;
+;   Artillery OS GRUB BootLoader    ;
+;                                   ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 [BITS 32]
+[global loader]
 [extern kernel_main]
-[global start]
 
-start:
-    mov esp, _sys_stack     ; This points the stack to our new stack area
-    jmp stublet
+; Setting up multibootheaders for GRUB
+MODULEALIGN equ 1<<0 ; align loaded modules on page boundary
 
-; This part MUST be 4byte aligned, so we solve that issue using 'ALIGN 4'
-ALIGN 4
-mboot:
-    ; Multiboot macros to make a few lines later more readable
-    MULTIBOOT_PAGE_ALIGN	equ 1<<0
-    MULTIBOOT_MEMORY_INFO	equ 1<<1
-    MULTIBOOT_AOUT_KLUDGE	equ 1<<16
-    MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
-    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
-    MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
-    EXTERN code, bss, end
+MEMINFO equ 1<<1 ; provide memory map
+FLAGS equ MODULEALIGN | MEMINFO
+MAGIC equ 0x1BADB002
 
-    ; This is the GRUB Multiboot header. A boot signature
-    dd MULTIBOOT_HEADER_MAGIC
-    dd MULTIBOOT_HEADER_FLAGS
-    dd MULTIBOOT_CHECKSUM
-    
-    ; AOUT kludge - must be physical addresses. Make a note of these:
-    ; The linker script fills in the data for these ones!
-    dd mboot
-    dd code
-    dd bss
-    dd end
-    dd start
+CHECKSUM equ -(MAGIC + FLAGS) ; checksum required
 
-; This is an endless loop here. Make a note of this: Later on, we
-; will insert an 'extern _main', followed by 'call _main', right
-; before the 'jmp $'.
-stublet:
+section .text
+
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+loader:
+    mov esp, stack+STACKSIZE ; set up the stack pointer
+    push eax ; eax holds the MAGIC number
+    push ebx ; ebx holds the Multiboot info
+
     call kernel_main
-    jmp $
 
+hang:
+    jmp hang ; loop forever
 
-; Shortly we will add code for loading the GDT right here!
+; reserving initial stack size
+STACKSIZE equ 0x4000 ; 16kB
 
-
-; In just a few pages in this tutorial, we will add our Interrupt
-; Service Routines (ISRs) right here!
-
-
-
-; Here is the definition of our BSS section. Right now, we'll use
-; it just to store the stack. Remember that a stack actually grows
-; downwards, so we declare the size of the data before declaring
-; the identifier '_sys_stack'
-SECTION .bss
-    resb 8192               ; This reserves 8KBytes of memory here
-_sys_stack:
+align 4
+stack:
+    resb STACKSIZE ; reserve memory for stack on doubleworded memory
