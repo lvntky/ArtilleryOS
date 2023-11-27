ab59cb6a51fcda17749cebe4a6494739d298249d
angelpilled <angelpilleddev@gmail.com>
2023-11-27 06:45:53 +0100

    Reverse function on line 68 was commented out inside stdio.c

--- kernel/libc/stdio.c
+++ kernel/libc/stdio.c
@@ -65,7 +65,7 @@ void ftoa(double num, char *buffer, int precision)
 	}
 
 	buffer[len + i] = '\0';
-	reverse(buffer, len + i);
+	// reverse(buffer, len + i);
 
 	// Add the negative sign if necessary
 	if (is_negative) {
